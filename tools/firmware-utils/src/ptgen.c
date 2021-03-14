kob/openwrt-xg040

commit bc70f1f8eec6eb6ea07af3329029cdba5e41df44

firmware-utils/ptgen: fix partition guid and name
guid and name function should be called before storing
partition info.

Signed-off-by: Oskari Lemmela <oskari@lemmela.net>

tools/firmware-utils/src/ptgen.c
@@ -529,7 +529,8 @@ static guid_t type_to_guid_and_name(unsigned char type, char **name)
 
 	switch (type) {
 		case 0xef:
-			*name = "EFI System Partition";
+			if(*name == NULL)
+				*name = "EFI System Partition";
 			guid = GUID_PARTITION_SYSTEM;
 			break;
 		case 0x83:
@@ -586,6 +587,7 @@ int main (int argc, char **argv)
 				*(p++) = 0;
 				parts[part].start = to_kbytes(p);
 			}
+			part_guid = type_to_guid_and_name(type, &name);
 			parts[part].size = to_kbytes(optarg);
 			parts[part].required = required;
 			parts[part].name = name;
@@ -598,7 +600,6 @@ int main (int argc, char **argv)
 			 */
 			name = NULL;
 			required = 0;
-			part_guid = type_to_guid_and_name(type, &name);
 			break;
 		case 'N':
 			name = optarg;

commit 04efcdd2b01e776df3c87afcc18ca7748e8be65d

firmware-utils/ptgen: remove unused variable assignment
Discovered by coverty:

 CID 1473630:  Code maintainability issues  (UNUSED_VALUE)

 Assigning value from "type_to_guid_and_name(type, &name)" to
 "part_guid" here, but that stored value is overwritten before it can
 be used.

Remove the now redundant assignment of part_guid which is also set
conditionally later on.

Fixes: bc70f1f8ee ("firmware-utils/ptgen: fix partition guid and name")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

tools/firmware-utils/src/ptgen.c
@@ -630,7 +630,6 @@ int main (int argc, char **argv)
 			break;
 		case 't':
 			type = (char)strtoul(optarg, NULL, 16);
-			part_guid = type_to_guid_and_name(type, &name);
 			break;
 		case 'a':
 			active = (int)strtoul(optarg, NULL, 0);