kob/openwrt-xg040

commit b3d63e000461436cccc1a0d899746d470d3bd0d3

kernel: 5.10: update sound modules
Update file paths for kmod-sound-hda-intel and reflect new dependency of
kmod-sound-hda-core on kmod-ledtrig-audio.

Reported-by: Javier Marcet <javier@marcet.info>
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

package/kernel/linux/modules/sound.mk
@@ -304,6 +304,10 @@ $(eval $(call KernelPackage,sound-dummy))
 define KernelPackage/sound-hda-core
   SUBMENU:=$(SOUND_MENU)
   TITLE:=HD Audio Sound Core Support
+  DEPENDS:= \
+	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-realtek:kmod-ledtrig-audio \
+	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-conexant:kmod-ledtrig-audio \
+	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-idt:kmod-ledtrig-audio
   KCONFIG:= \
 	CONFIG_SND_HDA_CORE \
 	CONFIG_SND_HDA_HWDEP=y \
@@ -522,7 +526,8 @@ define KernelPackage/sound-hda-intel
 	CONFIG_SND_HDA_INTEL
   FILES:= \
 	$(LINUX_DIR)/sound/pci/hda/snd-hda-intel.ko \
-	$(LINUX_DIR)/sound/hda/snd-intel-nhlt.ko
+	$(LINUX_DIR)/sound/hda/snd-intel-nhlt.ko@lt5.5 \
+	$(LINUX_DIR)/sound/hda/snd-intel-dspcfg.ko@ge5.5
   AUTOLOAD:=$(call AutoProbe,snd-hda-intel)
   $(call AddDepends/sound,kmod-sound-hda-core)
 endef

commit 1095f730cb127d9226739673d15d299a83f5b40e

kernel: robustify dependencies in kmod-sound-hda-core
Dependency tracking for kmod-sound-hda-core is fragile. Enabling some sound
codecs (Realtek, Conexant, Sigmatel) implicitly adds a kmod-ledtrig-audio
dependency, while an enabled kmod-ledtrig-audio can be picked up through
enabling others (e.g. kmod-sound-hda-intel), and the behaviour can change
across kernel versions.

As kmod-ledtrig-audio is under 2KB, make it an unconditional dependency.

Fixes: b3d63e000461 ("kernel: 5.10: update sound modules")
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

package/kernel/linux/modules/sound.mk
@@ -304,10 +304,7 @@ $(eval $(call KernelPackage,sound-dummy))
 define KernelPackage/sound-hda-core
   SUBMENU:=$(SOUND_MENU)
   TITLE:=HD Audio Sound Core Support
-  DEPENDS:= \
-	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-realtek:kmod-ledtrig-audio \
-	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-conexant:kmod-ledtrig-audio \
-	+LINUX_5_10&&PACKAGE_kmod-sound-hda-codec-idt:kmod-ledtrig-audio
+  DEPENDS:=+kmod-ledtrig-audio
   KCONFIG:= \
 	CONFIG_SND_HDA_CORE \
 	CONFIG_SND_HDA_HWDEP=y \