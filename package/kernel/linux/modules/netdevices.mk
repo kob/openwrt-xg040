kob/openwrt-xg040

commit 07101e39311b5786ca5b3985e4ca6402fd524efb

kernel: netdev: add qlcnic
Add driver for QLogic QLE8240 and QLE8242 Converged Ethernet devices.

Signed-off-by: Vieno Hakkerinen <vieno@hakkerinen.eu>

package/kernel/linux/modules/netdevices.mk
@@ -1193,6 +1193,23 @@ endef
 $(eval $(call KernelPackage,mlx5-core))
 
 
+define KernelPackage/qlcnic
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  DEPENDS:=@PCI_SUPPORT
+  TITLE:=QLogic QLE8240 and QLE8242 device support
+  KCONFIG:=CONFIG_QLCNIC
+  FILES:=$(LINUX_DIR)/drivers/net/ethernet/qlogic/qlcnic/qlcnic.ko
+  AUTOLOAD:=$(call AutoProbe,qlcnic)
+endef
+
+define KernelPackage/macvlan/description
+  This driver supports QLogic QLE8240 and QLE8242 Converged Ethernet
+  devices.
+endef
+
+$(eval $(call KernelPackage,qlcnic))
+
+
 define KernelPackage/sfp
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=SFP cage support

commit 7e12747d96564dd7d7c8e26617672f672e074ad3

kernel: qlcnic: fix typo in module description
Fixes: 07101e39311b ("kernel: netdev: add qlcnic")
Signed-off-by: Chukun Pan <amadeus@jmu.edu.cn>

package/kernel/linux/modules/netdevices.mk
@@ -1204,7 +1204,7 @@ define KernelPackage/qlcnic
   AUTOLOAD:=$(call AutoProbe,qlcnic)
 endef
 
-define KernelPackage/macvlan/description
+define KernelPackage/qlcnic/description
   This driver supports QLogic QLE8240 and QLE8242 Converged Ethernet
   devices.
 endef