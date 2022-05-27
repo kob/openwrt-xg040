kob/openwrt-xg040

commit b15f97d0f822e59f33567d2e42a5bf92a629afa5

ipq40xx: mikrotik: dont include ath10k-board-qca4019 by default
Since MikroTik subtarget now uses dynamic BDF loading its crucial that it
doesnt include the board-2.bin at all which is provided by the
ath10k-board-qca4019 package.

So to resolve this dont include the ath10k-board-qca4019 package on the
MikroTik subtarget.

Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/ipq40xx/mikrotik/target.mk
@@ -2,3 +2,4 @@ BOARDNAME:=MikroTik
 FEATURES += minor
 KERNEL_IMAGES:=vmlinux
 IMAGES_DIR:=compressed
+DEFAULT_PACKAGES += -ath10k-board-qca4019

commit 9a5ff60693c2ea534625f4b941747091c261e66f

ipq40xx: cut ath10k board file for mikrotik subtarget
Avoid shipping ath10k board file in Mikrotik initram images

Most will only ever need to use these initram images once—to initially
load OpenWrt, but fix these images for more consistent Wi-Fi performance
between the initram and installed squashfs images.

OpenWrt BUILDBOT config ignores -cut packages in the initram images build.
This results in BUILDBOT initram images including the linux-firmware
qca4019 board-2.bin, and (initram image booted) Mikrotik devices loading
a generic BDF, rather than the intended BDF data loaded
from NOR as an api 1 board_file.

buildbot snapshot booted as initram image:
cat /etc/openwrt_version
r19679-1338ad7e5d
dmesg | grep ath10k | grep -E board\|BDF
[    9.794556] ath10k_ahb a000000.wifi: Loading BDF type 0
[    9.807192] ath10k_ahb a000000.wifi: board_file api 2 bmi_id 0:16
crc32 11892f9b
[   12.457105] ath10k_ahb a800000.wifi: Loading BDF type 0
[   12.464945] ath10k_ahb a800000.wifi: board_file api 2 bmi_id 0:17
crc32 11892f9b

CC: Robert Marko <robimarko@gmail.com>
Fixes: b15f97d0f822 ("ipq40xx: mikrotik: dont include ath10k-board-qca4019 by default")

Signed-off-by: John Thomson <git@johnthomson.fastmail.com.au>
Reviewed-by: Robert Marko <robimarko@gmail.com>

target/linux/ipq40xx/Makefile
@@ -19,6 +19,6 @@ DEFAULT_PACKAGES += \
 	kmod-leds-gpio kmod-gpio-button-hotplug swconfig \
 	kmod-ath10k-ct wpad-basic-wolfssl \
 	kmod-usb3 kmod-usb-dwc3 ath10k-firmware-qca4019-ct \
-	ath10k-board-qca4019 uboot-envtools
+	uboot-envtools
 
 $(eval $(call BuildTarget))

target/linux/ipq40xx/chromium/target.mk
@@ -1,2 +1,3 @@
 BOARDNAME:=Google Chromium
 FEATURES += emmc boot-part rootfs-part
+DEFAULT_PACKAGES += ath10k-board-qca4019

target/linux/ipq40xx/generic/target.mk
@@ -1,2 +1,3 @@
 BOARDNAME:=Generic
 FEATURES+=emmc
+DEFAULT_PACKAGES += ath10k-board-qca4019

target/linux/ipq40xx/mikrotik/target.mk
@@ -2,4 +2,3 @@ BOARDNAME:=MikroTik
 FEATURES += minor
 KERNEL_IMAGES:=vmlinux
 IMAGES_DIR:=compressed
-DEFAULT_PACKAGES += -ath10k-board-qca4019