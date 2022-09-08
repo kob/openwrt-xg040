kob/openwrt-xg040

commit 83386a13ab81a395ad2dc28af0f61c504658ef75

mediatek: bpi-r64: make initramfs/recovery optional
Only include recovery image in SD card image generated for the
BananaPi BPi-R64 if building with CONFIG_TARGET_ROOTFS_INITRAMFS
This allows to build images larger than 32 MB (the limit for
initramfs/recovery image) by deselecting initramfs.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/mt7622.mk
@@ -86,13 +86,18 @@ define Device/bananapi_bpi-r64
   ARTIFACT/sdcard.img.gz	:= mt7622-gpt sdmmc |\
 				   pad-to 512k | bl2 sdmmc-2ddr |\
 				   pad-to 2048k | bl31-uboot bananapi_bpi-r64-sdmmc |\
-				   pad-to 6144k | append-image-stage initramfs-recovery.itb |\
+				$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
+				   pad-to 6144k | append-image-stage initramfs-recovery.itb | checksize 38912k |\
+				) \
 				   pad-to 38912k | mt7622-gpt emmc |\
 				   pad-to 39424k | bl2 emmc-2ddr |\
 				   pad-to 40960k | bl31-uboot bananapi_bpi-r64-emmc |\
 				   pad-to 43008k | bl2 snand-2ddr |\
 				   pad-to 43520k | bl31-uboot bananapi_bpi-r64-snand |\
-				   pad-to 46080k | append-image squashfs-sysupgrade.itb | gzip
+				$(if $(CONFIG_TARGET_ROOTFS_SQUASHFS),\
+				   pad-to 46080k | append-image squashfs-sysupgrade.itb | check-size | gzip \
+				)
+  IMAGE_SIZE := $$(shell expr 45 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
   KERNEL			:= kernel-bin | gzip
   KERNEL_INITRAMFS		:= kernel-bin | lzma | fit lzma $$(DTS_DIR)/$$(DEVICE_DTS).dtb with-initrd | pad-to 128k
   IMAGE/sysupgrade.itb		:= append-kernel | fit gzip $$(DTS_DIR)/$$(DEVICE_DTS).dtb external-static-with-rootfs | append-metadata

commit 483a56e5129fdfffc348a9bb5fa167cddfa1e7bf

mediatek: fix typo in bpi-r64 image recipe
Janusz Dziedzic reported a typo introduced by a recent commit. Fix it.

Fixes: 83386a13ab ("mediatek: bpi-r64: make initramfs/recovery optional")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/mt7622.mk
@@ -87,7 +87,7 @@ define Device/bananapi_bpi-r64
 				   pad-to 512k | bl2 sdmmc-2ddr |\
 				   pad-to 2048k | bl31-uboot bananapi_bpi-r64-sdmmc |\
 				$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
-				   pad-to 6144k | append-image-stage initramfs-recovery.itb | checksize 38912k |\
+				   pad-to 6144k | append-image-stage initramfs-recovery.itb | check-size 38912k |\
 				) \
 				   pad-to 38912k | mt7622-gpt emmc |\
 				   pad-to 39424k | bl2 emmc-2ddr |\