kob/openwrt-xg040

commit 3c7b1b9552f4a4f50efe53ab10c92fc16f16dfb0

x86: Add kmod-dwmac-intel
This adds the Intel Ethernet driver for the Intel Quark/EHL/TGL chips.

Fixes: #13994
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/netdevices.mk
@@ -1546,7 +1546,7 @@ $(eval $(call KernelPackage,sfp))
 define KernelPackage/stmmac-core
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)
-  DEPENDS:=@(TARGET_armsr_armv8) +kmod-pcs-xpcs +kmod-ptp \
+  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8 +kmod-pcs-xpcs +kmod-ptp \
     +kmod-of-mdio
   KCONFIG:=CONFIG_STMMAC_ETH \
     CONFIG_STMMAC_SELFTESTS=n \

target/linux/x86/modules.mk
@@ -18,6 +18,18 @@ endef
 $(eval $(call KernelPackage,amd-xgbe))
 
 
+define KernelPackage/dwmac-intel
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Intel GMAC support
+  DEPENDS:=@TARGET_x86_64 +kmod-stmmac-core
+  KCONFIG:=CONFIG_DWMAC_INTEL
+  FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-intel.ko
+  AUTOLOAD=$(call AutoLoad,45,dwmac-intel)
+endef
+
+$(eval $(call KernelPackage,dwmac-intel))
+
+
 define KernelPackage/f71808e-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Fintek F718xx/F818xx Watchdog Timer

target/linux/x86/image/64.mk
@@ -2,7 +2,7 @@ define Device/generic
   DEVICE_VENDOR := Generic
   DEVICE_MODEL := x86/64
   DEVICE_PACKAGES += \
-	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-e1000e kmod-e1000 \
+	kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 kmod-dwmac-intel kmod-e1000e kmod-e1000 \
 	kmod-forcedeth kmod-fs-vfat kmod-igb kmod-igc kmod-ixgbe kmod-r8169 \
 	kmod-tg3 kmod-mlxsw-core kmod-mlxsw-pci kmod-mlxsw-i2c \
   	kmod-mlxsw-spectrum kmod-mlxsw-minimal kmod-mlxfw \

commit 6f11c9c9347dd113c0ad8cf887586afda61b77ed

package: kernel: detach of-mdio dependency from stmmac-core
Detach of-mdio dependency from stmmac-core kmod to fix support for
x86_64 target. This target doesn't use OpenFirmware infrastructure and
stmmac-core for the dwmac-intel driver doesn't depends on it.

Add kmod-of-mdio to any other user of stmmac-core as it's not inherit
from stmmac-core anymore.

Fixes: #14209
Fixes: 3c7b1b9552f4 ("x86: Add kmod-dwmac-intel")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

package/kernel/linux/modules/netdevices.mk
@@ -1558,8 +1558,7 @@ $(eval $(call KernelPackage,pcs-xpcs))
 define KernelPackage/stmmac-core
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Synopsis Ethernet Controller core (NXP,STMMicro,others)
-  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8 +kmod-pcs-xpcs +kmod-ptp \
-    +kmod-of-mdio
+  DEPENDS:=@TARGET_x86_64||TARGET_armsr_armv8 +kmod-pcs-xpcs +kmod-ptp
   KCONFIG:=CONFIG_STMMAC_ETH \
     CONFIG_STMMAC_SELFTESTS=n \
     CONFIG_STMMAC_PLATFORM \

target/linux/armsr/modules.mk
@@ -208,7 +208,7 @@ $(eval $(call KernelPackage,imx7-ulp-wdt))
 define KernelPackage/dwmac-imx
   SUBMENU=$(NETWORK_DEVICES_MENU)
   TITLE:=NXP i.MX8 Ethernet controller
-  DEPENDS:=+kmod-stmmac-core
+  DEPENDS:=+kmod-stmmac-core +kmod-of-mdio
   KCONFIG:=CONFIG_DWMAC_IMX8
   FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-imx.ko
   AUTOLOAD=$(call AutoLoad,45,dwmac-imx)
@@ -219,7 +219,7 @@ $(eval $(call KernelPackage,dwmac-imx))
 define KernelPackage/dwmac-sun8i
   SUBMENU=$(NETWORK_DEVICES_MENU)
   TITLE:=Allwinner H3/A83T/A64 (sun8i) Ethernet
-  DEPENDS:=+kmod-stmmac-core +kmod-mdio-bus-mux
+  DEPENDS:=+kmod-stmmac-core +kmod-of-mdio +kmod-mdio-bus-mux
   KCONFIG:=CONFIG_DWMAC_SUN8I
   FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.ko
   AUTOLOAD=$(call AutoLoad,45,dwmac-sun8i)
@@ -230,7 +230,7 @@ $(eval $(call KernelPackage,dwmac-sun8i))
 define KernelPackage/dwmac-rockchip
   SUBMENU=$(NETWORK_DEVICES_MENU)
   TITLE:=Rockchip RK3328/RK3399/RK3568 Ethernet
-  DEPENDS:=+kmod-stmmac-core +kmod-mdio-bus-mux
+  DEPENDS:=+kmod-stmmac-core +kmod-of-mdio +kmod-mdio-bus-mux
   KCONFIG:=CONFIG_DWMAC_ROCKCHIP
   FILES=$(LINUX_DIR)/drivers/net/ethernet/stmicro/stmmac/dwmac-rk.ko
   AUTOLOAD=$(call AutoLoad,45,dwmac-rk)