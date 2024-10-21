Arastys/openwrt

bcm53xx: add support for Cisco Meraki MX64/MX65
This commit adds support for the Cisco Meraki MX64 and MX65 devices which
use the Broadcom NSP SoC, which is compatible with the bcm53xx platform.

MX64 Hardware info:
  - CPU: Broadcom BCM58625 Cortex A9 @ 1200Mhz
  - RAM: 2 GB (4 x 4Gb SK Hynix H5TC4G83CFR)
  - Storage: 1 GB (Micron MT29F8G08ABACA)
  - Networking: BCM58625 internal switch (5x 1GbE ports)
  - USB: 1x USB2.0
  - Serial: Internal header

MX65 Hardware info:
  - CPU: Broadcom BCM58625 Cortex A9 @ 1200Mhz
  - RAM: 2 GB (4 x 4Gb SK Hynix H5TC4G83CFR)
  - Storage: 1 GB (Micron MT29F8G08ABACA)
  - Networking: BCM58625 switch (2x 1GbE ports, used for WAN ports 1 & 2)
    2x Qualcomm QCA8337 switches (10x 1GbE ports, used for LAN ports 3-12)
  - PSE: Broadcom BCM59111KMLG connected to LAN ports 11 & 12
  - USB: 1x USB2.0
  - Serial: Internal header

Included are the following changes:
  - NSP config addition for kernel 5.10.
  - Uses modified mkmerakifw-old to handle LE mx64/mx65 in addition to
    the existing BE z1. The "bootbk" header format is otherwise the same
    on both systems. The header is checked when booting, gracefully
    falling back to the u-boot cli on failure.
  - "ARM: BCM5301X: Disable MMU and Dcache during decompression": This
    patch is also required on this platform.

Notes:
  - The meraki provided GPL source are available at [2].
  - Wireless capability on the MX64W and MX65W exists in the form of 2x
    Broadcom BCM43520KMLG, which is not supported. These devices will work
    otherwise as standard MX64 or MX65 devices.
  - Early MX64 units use an A0 variant of the BCM958625 SoC which lacks
    cache coherency and uses a different "secondary-boot-reg". As a
    consequence a different device tree is needed.
  - Installation of OpenWrt requires changing u-boot to a custom version.
    This is due to the stock u-boot "nand read" command being limited to
    load only 2MB, in spite of the bootkernel1 and bootkernel2 partitions
    both

target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
@@ -195,7 +195,10 @@ platform_other_check_image() {
 
 platform_check_image() {
 	case "$(board_name)" in
-	meraki,mr32)
+	meraki,mr32|\
+	meraki,mx64|\
+	meraki,mx64-a0|\
+	meraki,mx65)
 		# NAND sysupgrade
 		return 0
 		;;
@@ -395,6 +398,13 @@ platform_do_upgrade() {
 	meraki,mr32)
 		REQUIRE_IMAGE_METADATA=1
 		CI_KERNPART="part.safe"
+		nand_do_upgrade "$1"
+		;;
+	meraki,mx64|\
+	meraki,mx64-a0|\
+	meraki,mx65)
+		REQUIRE_IMAGE_METADATA=1
+		CI_KERNPART="bootkernel1"
 		nand_do_upgrade "$1"
 		;;
 	*)

kob/openwrt-xg040

  - The Meraki provided GPL source are available at [2].
  - Initial work on this device used a small bootloader within the OEM
    partition scheme. To allow booting of larger kernels, UBI and bootm
    support has been added, along with ability to store env variables to
    the NAND. The Shmoo and newly created env partitions have been moved
    to the extra space available after the nvram data.
  - Users who installed the previous non-UBI supporting bootloader will
    need to convert to the new one before flashing a compatible image.
    These steps are detailed below.
  1. Compile or obtain OpenWrt files for the MX64 or MX65, including
     u-boot[3][4], initramfs and sysupgrade images.
  2. A USB disk with DOS partition scheme and primary FAT partition is
     required.
  3. If installing onto an MX64, set up a local web server.
  4. On the device, boot into diagnostic mode by holding reset when
  5. Plug an Ethernet cable into the first LAN port, set the host to
     steps 4 and 5 in this section. If the result is 0 then skip these.
  3  Confirm the size of the device's boot(mtd0) partition. In most
     cases it should be 0x100000 or larger. If this is the case, please
     proceed to use the uboot_mx64 image. If the reported size is
     0x80000, please use the uboot_mx64_small image, then follow the
     later guide to change to the larger image.
        `cat /proc/mtd`
     Example output:
        `# cat /proc/mtd
        cat /proc/mtd
        dev:    size   erasesize  name
        mtd0: 00100000 00040000 "boot"
        mtd1: 00080000 00040000 "shmoo"
        mtd2: 00300000 00040000 "bootkernel1"
        mtd3: 00100000 00040000 "nvram"
        mtd4: 00300000 00040000 "bootkernel2"
        mtd5: 3f700000 00040000 "ubi"
        mtd6: 40000000 00040000 "all"`
  4. Set up a webserver to serve the appropriate uboot_mx64 from the
     following location and verify the SHA512:
  5. (Only if mtd0 is locked) You will also need the mtd-rw.ko kernel
  6. (Only if mt

target/linux/bcm53xx/base-files/lib/upgrade/platform.sh
@@ -200,7 +200,10 @@ platform_check_image() {
 	board="$(board_name)"
 	case "$board" in
 	meraki,mr26 | \
-	meraki,mr32)
+	meraki,mr32 | \
+	meraki,mx64 | \
+	meraki,mx64-a0 | \
+	meraki,mx65)
 		# NAND sysupgrade
 		return 0
 		;;
@@ -401,6 +404,12 @@ platform_do_upgrade() {
 	meraki,mr32)
 		REQUIRE_IMAGE_METADATA=1
 		CI_KERNPART="part.safe"
+		nand_do_upgrade "$1"
+		;;
+	meraki,mx64 | \
+	meraki,mx64-a0 | \
+	meraki,mx65)
+		REQUIRE_IMAGE_METADATA=1
 		nand_do_upgrade "$1"
 		;;
 	*)