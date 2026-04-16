kob/openwrt-xg040

commit 801cf511448f7b90b144bb7ccc42af762176520b

treewide: use of_device_get_match_data
Simplifies code slightly.

Signed-off-by: Rosen Penev <rosenp@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/21599
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/lantiq/ltq-atm/src/ltq_atm.c
@@ -38,7 +38,7 @@
 #include <linux/atmdev.h>
 #include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
 #include <linux/atm.h>
 #include <linux/clk.h>
 #include <linux/interrupt.h>
@@ -1756,19 +1756,11 @@ MODULE_DEVICE_TABLE(of, ltq_atm_match);
 
 static int ltq_atm_probe(struct platform_device *pdev)
 {
-	const struct of_device_id *match;
-	struct ltq_atm_ops *ops = NULL;
+	const struct ltq_atm_ops *ops;
 	int ret;
 	int port_num;
 	struct port_cell_info port_cell = {0};
 	char ver_str[256];
-
-	match = of_match_device(ltq_atm_match, &pdev->dev);
-	if (!match) {
-		dev_err(&pdev->dev, "failed to find matching device\n");
-		return -ENOENT;
-	}
-	ops = (struct ltq_atm_ops *) match->data;
 
 	check_parameters();
 
@@ -1778,6 +1770,7 @@ static int ltq_atm_probe(struct platform_device *pdev)
 		goto INIT_PRIV_DATA_FAIL;
 	}
 
+	ops = of_device_get_match_data(&pdev->dev);
 	ret = ops->init(pdev);
 	if (ret)
 		return ret;

package/kernel/lantiq/ltq-atm/Makefile
@@ -7,7 +7,7 @@ include $(TOPDIR)/rules.mk
 include $(INCLUDE_DIR)/kernel.mk
 
 PKG_NAME:=ltq-atm
-PKG_RELEASE:=3
+PKG_RELEASE:=4
 
 PKG_MAINTAINER:=John Crispin <john@phrozen.org>
 PKG_LICENSE:=GPL-2.0+

target/linux/generic/files/drivers/net/phy/ar8216.c
@@ -23,7 +23,7 @@
 #include <linux/skbuff.h>
 #include <linux/netdevice.h>
 #include <linux/netlink.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
 #include <linux/of_mdio.h>
 #include <linux/of_net.h>
 #include <linux/bitops.h>
@@ -2757,15 +2757,10 @@ static const struct of_device_id ar8xxx_mdiodev_of_match[] = {
 static int
 ar8xxx_mdiodev_probe(struct mdio_device *mdiodev)
 {
-	const struct of_device_id *match;
 	struct ar8xxx_priv *priv;
 	struct switch_dev *swdev;
 	struct device_node *mdio_node;
 	int ret;
-
-	match = of_match_device(ar8xxx_mdiodev_of_match, &mdiodev->dev);
-	if (!match)
-		return -EINVAL;
 
 	priv = ar8xxx_create();
 	if (priv == NULL)
@@ -2773,7 +2768,7 @@ ar8xxx_mdiodev_probe(struct mdio_device *mdiodev)
 
 	priv->mii_bus = mdiodev->bus;
 	priv->pdev = &mdiodev->dev;
-	priv->chip = (const struct ar8xxx_chip *) match->data;
+	priv->chip = of_device_get_match_data(&mdiodev->dev);
 
 	ret = of_property_read_u32(priv->pdev->of_node, "qca,mib-poll-interval",
 				   &priv->mib_poll_interval);

target/linux/generic/files/drivers/net/phy/ar8327.c
@@ -23,7 +23,6 @@
 #include <linux/lockdep.h>
 #include <linux/workqueue.h>
 #include <linux/of.h>
-#include <linux/of_device.h>
 #include <linux/leds.h>
 #include <linux/mdio.h>
 

target/linux/qualcommax/patches-6.12/0805-remoteproc-qcom-Add-Hexagon-based-multipd-rproc-driv.patch
@@ -96,7 +96,7 @@ Signed-off-by: Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
  obj-$(CONFIG_QCOM_Q6V5_WCSS)		+= qcom_q6v5_wcss.o
 --- /dev/null
 +++ b/drivers/remoteproc/qcom_q6v5_mpd.c
-@@ -0,0 +1,801 @@
+@@ -0,0 +1,796 @@
 +// SPDX-License-Identifier: GPL-2.0
 +/*
 + * Copyright (C) 2016-2018 Linaro Ltd.
@@ -788,17 +788,12 @@ Signed-off-by: Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
 +
 +static int q6_wcss_probe(struct platform_device *pdev)
 +{
-+	const struct wcss_data *desc;
 +	struct q6_wcss *wcss;
 +	struct rproc *rproc;
 +	int ret;
 +	const char **firmware;
 +	struct device_node *userpd_np;
 +	const struct rproc_ops *ops = &q6_wcss_ops;
-+
-+	desc = of_device_get_match_data(&pdev->dev);
-+	if (!desc)
-+		return -EINVAL;
 +
 +	firmware = devm_kcalloc(&pdev->dev, MAX_FIRMWARE,
 +				sizeof(*firmware), GFP_KERNEL);
@@ -817,7 +812,7 @@ Signed-off-by: Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
 +
 +	wcss = rproc->priv;
 +	wcss->dev = &pdev->dev;
-+	wcss->desc = desc;
++	wcss->desc = of_device_get_match_data(&pdev->dev);
 +	wcss->firmware = firmware;
 +	wcss->version = Q6_IPQ;
 +

target/linux/qualcommax/patches-6.12/0806-rproc-qcom_q6v5_mpd-split-q6_wcss-to-rootpd-and-user.patch
@@ -311,9 +311,9 @@ Signed-off-by: George Moussalem <george.moussalem@outlook.com>
  	return 0;
  
  free_rproc:
-@@ -720,7 +707,6 @@ static int q6_wcss_probe(struct platform
+@@ -715,7 +702,6 @@ static int q6_wcss_probe(struct platform
  	wcss->dev = &pdev->dev;
- 	wcss->desc = desc;
+ 	wcss->desc = of_device_get_match_data(&pdev->dev);
  	wcss->firmware = firmware;
 -	wcss->version = Q6_IPQ;
  

target/linux/qualcommax/patches-6.12/0807-remoteproc-qcom_q6v5_mpd-fix-incorrent-use-of-rproc-.patch
@@ -191,7 +191,7 @@ Signed-off-by: Ziyang Huang <hzyitc@outlook.com>
  	platform_set_drvdata(userpd_pdev, rproc);
  	qcom_add_ssr_subdev(rproc, &upd->ssr_subdev, userpd_pdev->name);
  	return 0;
-@@ -729,10 +736,10 @@ static int q6_wcss_probe(struct platform
+@@ -724,10 +731,10 @@ static int q6_wcss_probe(struct platform
  
  	/* Iterate over userpd child's and register with rproc */
  	for_each_available_child_of_node(pdev->dev.of_node, userpd_np) {

target/linux/qualcommax/patches-6.12/0813-remoteproc-qcom_q6v5_mpd-enable-clocks.patch
@@ -19,7 +19,7 @@ Signed-off-by: Ziyang Huang <hzyitc@outlook.com>
  	const struct wcss_data *desc;
  	const char **firmware;
  	struct userpd *upd[MAX_UPD];
-@@ -719,6 +721,16 @@ static int q6_wcss_probe(struct platform
+@@ -714,6 +716,16 @@ static int q6_wcss_probe(struct platform
  	if (ret)
  		goto free_rproc;
  

commit 2ebad3c0e15c02299427d5f7af281bfde9651f2f

kernel: kmod-ltq-atm: fix discarded-qualifiers build error
Fixes:

ltq_atm.c: In function 'ltq_atm_probe':
ltq_atm.c:1840:36: error: passing argument 2 of 'platform_set_drvdata' discards 'const' qualifier from pointer target type [-Werror=discarded-qualifiers]
 1840 |         platform_set_drvdata(pdev, ops);
      |                                    ^~~
In file included from ltq_atm.c:40:
/workspaces/openwrt/build_dir/target-mips_24kc_musl/linux-lantiq_xway/linux-6.18.21/include/linux/platform_device.h:276:47: note: expected 'void *' but argument is of type 'const struct ltq_atm_ops *'
  276 |                                         void *data)
      |                                         ~~~~~~^~~~

Fixes: 801cf511448f ("treewide: use of_device_get_match_data")
Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/22921
Signed-off-by: Robert Marko <robimarko@gmail.com>

package/kernel/lantiq/ltq-atm/src/ltq_atm.c
@@ -1837,7 +1837,7 @@ static int ltq_atm_probe(struct platform_device *pdev)
 
 	ifx_atm_version(ops, ver_str);
 	printk(KERN_INFO "%s", ver_str);
-	platform_set_drvdata(pdev, ops);
+	platform_set_drvdata(pdev, (void *)ops);
 	printk("ifxmips_atm: ATM init succeed\n");
 
 	return 0;