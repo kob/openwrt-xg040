kob/openwrt-xg040

commit 0d0b6aea3dbb311d2dbc4b4919f53558cc2daf81

ath79: mikrotik: compile SWCONFIG and AR8216_PHY as modules
Unset the SWCONFIG symbol and AR8216_PHY which selects SWCONFIG. Add
kmod-switch-ar8xxx, which enables AR8216_PHY, to DEFAULT_PACKAGES for the
subtarget. With this, swconfig driver will be now compiled as a module, as
kmod-switch-ar8xxx selects kmod-swconfig.

Refresh the config-default file for ath79/mikrotik while at it.

This change makes it possible to disable the swconfig driver for MikroTik
RouterBOARD 960PGS (hEX PoE/PowerBox Pro).

Signed-off-by: Chester A. Unal <chester.a.unal@arinc9.com>

target/linux/ath79/mikrotik/config-default
@@ -1,3 +1,4 @@
+# CONFIG_AR8216_PHY is not set
 CONFIG_CRC16=y
 CONFIG_CRYPTO_DEFLATE=y
 CONFIG_CRYPTO_HASH_INFO=y
@@ -38,6 +39,7 @@ CONFIG_NET_DSA=y
 CONFIG_PHYLINK=y
 CONFIG_REGULATOR_FIXED_VOLTAGE=y
 CONFIG_SPI_RB4XX=y
+# CONFIG_SWCONFIG is not set
 CONFIG_UBIFS_FS=y
 CONFIG_WATCHDOG_CORE=y
 CONFIG_XXHASH=y

target/linux/ath79/mikrotik/target.mk
@@ -3,7 +3,7 @@ FEATURES += minor nand
 KERNELNAME := vmlinux vmlinuz
 IMAGES_DIR := ../../..
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls yafut
+DEFAULT_PACKAGES += wpad-basic-mbedtls yafut kmod-switch-ar8xxx
 
 define Target/Description
 	Build firmware images for MikroTik devices based on Qualcomm Atheros

commit 1dfb64873cd76306c61cd302371a0fc3c8724275

ath79: mikrotik: also compile AG71XX_LEGACY as a module
Commit 0d0b6aea3dbb ("ath79: mikrotik: compile SWCONFIG and AR8216_PHY
as modules") caused the hybrid PHY/MDIO ar8xxx driver to be built as a
module instead of built-in.  On at least MikroTik RouterBOARD 951G-2HnD,
this prevents the kernel from binding the correct PHY driver at boot
because the ar8xxx driver is not yet available when the MDIO bus is
probed:

  - before:

    ag71xx-legacy 19000000.eth: invalid MAC address, using random address
    switch0: Atheros AR8327 rev. 4 switch registered on mdio.0
    ag71xx-legacy 19000000.eth: connected to PHY at mdio.0:00 [uid=004dd034, driver=Atheros AR8216/AR8236/AR8316]
    eth0: Atheros AG71xx at 0xb9000000, irq 4, mode: rgmii

  - after:

    ag71xx-legacy 19000000.eth: invalid MAC address, using random address
    ag71xx-legacy 19000000.eth: connected to PHY at mdio.0:00 [uid=004dd034, driver=Generic PHY]
    eth0: Atheros AG71xx at 0xb9000000, irq 4, mode: rgmii

As the PHY is already bound to the fallback "Generic PHY" driver when
the ar8xxx module is loaded, the switch remains non-functional and the
router has no network connectivity.

Fix by also compiling the MAC driver (ag71xx-legacy) as a module,
deferring switch initialization until control is transferred to
userspace during boot.  The configured module autoload priorities
(ar8xxx: 43, ag71xx-legacy: 50) ensure that the ar8xxx driver is loaded
before ag71xx-legacy.

Fixes: https://github.com/openwrt/openwrt/issues/23739
Fixes: 0d0b6aea3dbb ("ath79: mikrotik: compile SWCONFIG and AR8216_PHY as modules")
Suggested-by: Daniel Golle <daniel@makrotopia.org>
Signed-off-by: Michał Kępień <openwrt@kempniu.pl>
Link: https://github.com/openwrt/openwrt/pull/23748
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/ath79/mikrotik/config-default
@@ -1,3 +1,4 @@
+# CONFIG_AG71XX_LEGACY is not set
 # CONFIG_AR8216_PHY is not set
 CONFIG_CRC16=y
 CONFIG_CRYPTO_DEFLATE=y

target/linux/ath79/mikrotik/target.mk
@@ -3,7 +3,7 @@ FEATURES += minor nand
 KERNELNAME := vmlinux vmlinuz
 IMAGES_DIR := ../../..
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls yafut kmod-switch-ar8xxx
+DEFAULT_PACKAGES += kmod-ag71xx-legacy kmod-switch-ar8xxx wpad-basic-mbedtls yafut
 
 define Target/Description
 	Build firmware images for MikroTik devices based on Qualcomm Atheros