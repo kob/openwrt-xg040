kob/openwrt-xg040

commit ac658028d2e57359d7ffc378810d2ce46442b699

airoha: an7583: remove source-only flag
Remove source-only flag from Airoha an7583 subtarget for upcoming Nokia
XG-040G-MF support. Also disable builds for airoha_an7583-evb and
airoha_an7583-evb-emmc evaluation boards.

Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/23809
Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

target/linux/airoha/image/an7583.mk
@@ -15,6 +15,7 @@ define Device/airoha_an7583-evb
   DEVICE_DTS_CONFIG := config@1
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | \
 	pad-rootfs | append-metadata
+  DEFAULT := n
 endef
 TARGET_DEVICES += airoha_an7583-evb
 
@@ -23,5 +24,6 @@ define Device/airoha_an7583-evb-emmc
   DEVICE_MODEL := AN7583 Evaluation Board (EMMC)
   DEVICE_DTS := an7583-evb-emmc
   DEVICE_PACKAGES := kmod-phy-airoha-en8811h kmod-i2c-an7581
+  DEFAULT := n
 endef
 TARGET_DEVICES += airoha_an7583-evb-emmc

target/linux/airoha/an7583/target.mk
@@ -3,7 +3,7 @@ SUBTARGET:=an7583
 BOARDNAME:=AN7583
 CPU_TYPE:=cortex-a53
 KERNELNAME:=Image dtbs
-FEATURES+=pwm source-only
+FEATURES+=pwm
 
 DEFAULT_PACKAGES += \
 	airoha-an7583-npu-firmware kmod-leds-gpio \

commit c067ea85beeb6894ab571bfbb4e0997917110cb7

airoha: an7583: build airoha_an7583-evb by default
These devices aren't broken and their build shouldn't have been disabled.

Fixes: ac65802 ("airoha: an7583: remove source-only flag")
Link: https://github.com/openwrt/openwrt/pull/23809#issuecomment-5011321092
Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/24285
Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

target/linux/airoha/image/an7583.mk
@@ -15,7 +15,6 @@ define Device/airoha_an7583-evb
   DEVICE_DTS_CONFIG := config@1
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | \
 	pad-rootfs | append-metadata
-  DEFAULT := n
 endef
 TARGET_DEVICES += airoha_an7583-evb
 
@@ -24,7 +23,6 @@ define Device/airoha_an7583-evb-emmc
   DEVICE_MODEL := AN7583 Evaluation Board (EMMC)
   DEVICE_DTS := an7583-evb-emmc
   DEVICE_PACKAGES := kmod-phy-airoha-en8811h kmod-i2c-an7581
-  DEFAULT := n
 endef
 TARGET_DEVICES += airoha_an7583-evb-emmc
 